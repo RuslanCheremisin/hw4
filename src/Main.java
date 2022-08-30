591983bf2605d543db1c412661148664aff03cf9
rus <ruslan.cheremisin@gmail.com>
2022-08-31 00:27:14 +1000

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
